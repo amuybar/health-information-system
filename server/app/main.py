4d594e9fe52403552ec2f1d318e77a8be71a36c5
amuybar <amuyunzubq@gmail.com>
2025-04-25 09:00:27 +0300

    feat(server): update CORS middleware to allow all origins and methods

--- server/app/main.py
+++ server/app/main.py
@@ -39,12 +39,9 @@ app = FastAPI(
 # Configure CORS middleware to allow requests from specified origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost",
-        "http://localhost:5173", 
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
